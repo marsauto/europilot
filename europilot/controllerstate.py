31ba11495537bd4e1b47d86bfcb066218eddb975
Gyuri Im <im9uri@gmail.com>
2017-07-21 15:50:47 +0900

    add controllerstate to get controller data

---
+++ europilot/controllerstate.py
@@ -0,0 +1,113 @@
+import sys
+from subprocess import PIPE, Popen
+from threading import Thread
+from Queue import Queue
+import json
+from collections import OrderedDict
+
+
+class ControllerOutput(object):
+    """
+    Reads stdout from G27.py, to get the controller event outputs without
+    blocking.
+    """
+    def __init__(self):
+        ON_POSIX = 'posix' in sys.builtin_module_names
+        self.p = Popen(['python', 'G27.py'], bufsize=1, stdout=PIPE,
+                       close_fds=ON_POSIX)
+        self.q = Queue()
+        self.t = Thread(target=self.__enqueue_output,
+                        args=(self.p, self.q))
+        self.t.daemon = True  # thread dies with program
+
+    def start(self):
+        self.t.start()
+
+    def __enqueue_output(self, process, queue):
+        """Insert stdout into queue"""
+        for line in iter(process.stdout.readline, ''):
+            print("line: %s" % line)
+            queue.put(line)
+        process.stdout.close()
+
+    def get_output(self):
+        """Append all of the items in the queue
+            >>> c.get_output()
+            >>> ['wheel-axis  256', 'wheel-axis  252', 'wheel-axis  244']
+        """
+        message = []
+        for _ in range(0, self.q.qsize()):
+            message.append(self.q.get().strip())
+        return message
+
+
+class ControllerState(object):
+    """
+    Holds the latest value of each of the controller output value.
+        >>> c = ControllerState()
+        >>> c.start()
+        >>> c.get_state()
+        OrderedDict([('wheel-axis', '1012'), ('clutch', '-27865'),...]
+    """
+    def __init__(self):
+        self.output = ControllerOutput()
+        self.state = self.__init_dict()
+
+    def start(self):
+        self.output.start()
+
+    def __init_dict(self):
+        """Initialize the values for each of the controller output"""
+        d = OrderedDict()
+        d['wheel-axis'] = 0
+        d['clutch'] = -32767
+        d['brake'] = -32767
+        d['gas'] = -32767
+        d['paddle-left'] = 0
+        d['paddle-right'] = 0
+        d['wheel-button-left-1'] = 0
+        d['wheel-button-left-2'] = 0
+        d['wheel-button-left-3'] = 0
+        d['wheel-button-right-1'] = 0
+        d['wheel-button-right-2'] = 0
+        d['wheel-button-right-3'] = 0
+        d['shifter-button-left'] = 0
+        d['shifter-button-right'] = 0
+        d['shifter-button-up'] = 0
+        d['shifter-button-down'] = 0
+        d['dpad-left/right'] = 0
+        d['dpad-up/down'] = 0
+        d['shifter-button-1'] = 0
+        d['shifter-button-2'] = 0
+        d['shifter-button-3'] = 0
+        d['shifter-button-4'] = 0
+        d['gear-1'] = 0
+        d['gear-2'] = 0
+        d['gear-3'] = 0
+        d['gear-4'] = 0
+        d['gear-5'] = 0
+        d['gear-6'] = 0
+        d['gear-R'] = 0
+
+        return d
+
+    def __update_state(self):
+        """Update ControllerState with the latest controller data"""
+        o = self.output.get_output()
+        # iterate through all of the stdout from beginning to end.
+        for msg in o:
+            msg = msg.split(' ')
+            k, v = msg[0], msg[1]
+            if k in self.state:
+                self.state[k] = v
+
+    def get_state(self):
+        """returns the latest state"""
+        self.__update_state()
+        return self.state
+
+    def get_state_json(self):
+        """returns the latest state in json format"""
+        self.__update_state()
+        j = json.dumps(self.state)
+        return j
---
+++ europilot/g27.py
@@ -0,0 +1,200 @@
+"""
+http://upgrayd.blogspot.de/2011/03/logitech-dual-action-usb-gamepad.html
+
+G27
+===
+example::
+
+    A0 B7 A3 04 5C 7D 02 02
+     0  1  2  3  4  5  6  7
+
+
+    0, 1, 2, 3: sequence, little endian
+    4, 5: value, little endian
+    6: group
+    7: axis
+
+NOTE! From here on, I talk big endian -- also in hex!
+
+Wheel values::
+
+            left               dead           right
+            <-------------------XX---------------->
+    dec     32769      65535     0      1     32767
+    hex     80 01      ff ff  00 00 00 01     7F FF
+
+
+Pedal values::
+
+- no pressure: 7F FF
+- halfway: 00 00
+- full: 80 01
+
+
+Button values::
+
+- on: 01 00
+- off: 00 00
+
+
+Gear values::
+
+- on: 01 00
+- off: 00 00
+
+
+Arrow Pad values::
+
+- left/up: 01 80 (32769)
+- right/down: ff 7f (32767)
+- off : 00 00
+
+
+For wheel,pedal values, the int output range is normalized to [-32767, 32767]
+For Arrow Pad values, the int output range is normalized to [-1, 1]
+
+
+"""
+from binascii import hexlify
+
+BUTTON_NAME = """
+0200=wheel-axis
+0201=clutch
+0203=brake
+0202=gas
+0105=paddle-left
+0104=paddle-right
+0107=wheel-button-left-1
+0114=wheel-button-left-2
+0115=wheel-button-left-3
+0106=wheel-button-right-1
+0112=wheel-button-right-2
+0113=wheel-button-right-3
+0101=shifter-button-left
+0102=shifter-button-right
+0103=shifter-button-up
+0100=shifter-button-down
+0204=dpad-left/right
+0205=dpad-up/down
+010b=shifter-button-1
+0108=shifter-button-2
+0109=shifter-button-3
+010a=shifter-button-4
+010c=gear-1
+010d=gear-2
+010e=gear-3
+010f=gear-4
+0110=gear-5
+0111=gear-6
+0116=gear-R
+"""
+
+
+class Bytewurst(object):
+
+    def __init__(self, bs):
+        self.raw = bs
+        self.ints = map(ord, bs)
+
+    def __repr__(self):
+        return ' '.join(map(hexlify, self.raw))
+
+    @property
+    def int(self):
+        """
+        For "01 0A" ints would be [1, 10], so::
+            >>> bs = '\x01\x0A'
+            >>> bw = Bytewurst(bs)
+            >>> bw.int == (1 * 1) + (10 * 256)
+        """
+
+        def powergenerator(start=0):
+            """Generate powers of 256"""
+            i = start
+            while True:
+                yield 256 ** i
+                i += 1
+
+        return sum(a * b for a, b in zip(self.ints, powergenerator()))
+
+    @property
+    def hexLE(self):
+        return hexlify(self.raw)
+
+    @property
+    def bits(self):
+        return ' '.join([format(x, '08b') for x in self.ints])
+
+
+class Button(Bytewurst):
+    def __init__(self, bs):
+        super(Button, self).__init__(bs)
+        button_namedict = dict(line.split('=') for line in
+                               BUTTON_NAME.strip().split('\n'))
+        self.name = button_namedict.get(self.hexLE, 'UNKNOWN: %s' % self.hexLE)
+
+
+class Value(Bytewurst):
+    def __repr__(self):
+        if self.int == 0:
+            return ' off'
+        elif self.int == 1:
+            return ' on'
+        else:
+            return ' %d' % self.int
+
+    def int_normalized(self, name):
+        """ Normalizes value to an adequate range
+
+        For wheel, pedal values, the int output range is normalized to
+        [-32767,32767]
+        For Arrow Pad values, the int output range is normalized to [-1, 1]
+        """
+        v = super(Value, self).int
+        if name == 'wheel-axis':
+            if v >= 32769:
+                v = v - 65536
+        elif name == 'clutch' or name == 'brake' or name == 'gas':
+            if v >= 32769:
+                v = -v + 65536
+            else:
+                v = -v
+        elif name == 'dpad-left/right' or name == 'dpad-up/down':
+            if v == 32769:
+                v = -1
+            elif v == 32767:
+                v = 1
+
+        return v
+
+
+class Message(object):
+
+    FMT_HEX = '%02X'
+    FMT_DEC = '%03d'
+
+    def __init__(self, bs):
+        self.bs = bs
+        self.raw_seq = bs[0:4]
+        self.raw_value = bs[4:6]
+        self.raw_id = bs[6:8]
+        self.ints = map(ord, bs)
+        self.sequence = Bytewurst(bs[0:4])
+        self.value = Value(bs[4:6])
+        self.button = Button(bs[6:8])
+
+    def __repr__(self):
+        values = (self.button.name, self.value.int_normalized(self.button.name))
+        return ' '.join(map(str, values))
+
+
+def dump_messages(input):
+    with open(input, 'rb') as device:
+        while True:
+            bs = device.read(8)
+            message = Message(bs)
+            print message
+
+
+if __name__ == '__main__':
+    dump_messages('/dev/input/js0')
