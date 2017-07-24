33575af0fd40abd4bf493d6186fe42abeafb32b9
daftshady <daftonshady@gmail.com>
2017-07-24 15:07:19 +0900

    removed model related dependencies

--- setup.py
+++ setup.py
@@ -3,18 +3,8 @@ from setuptools import setup
 
 
 install_requires = [
-    'opencv_python>=3.2.0.7',
-    'Keras>=2.0.4',
-    'matplotlib>=1.3.1',
     'mss==3.0.1',
     'numpy>=1.12.1',
-    'pandas>=0.19.0',
-    'scipy>=0.17.1',
-    'Theano>=0.8.2',
-    'ipython>=5.0.0,<5.4.1',
-    'Pillow>=4.2.1',
-    'bcolz>=1.1.2',
-    'scikit_learn>=0.19b2'
 ]
 
 about = {}
